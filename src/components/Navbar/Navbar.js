301d1baddfc57afb6c23f450e1d7f1ad2372e5ac
tomylandolfi <tomaslandolfi@gmail.com>
2022-05-02 23:36:49 -0300

    Creado el NavBar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,28 @@
+
+
+export default function Navbar(){
+    const ulStyles = {
+        display : "flex",
+        listStyleType: "none",
+        position : "relative",
+        left: "80%",
+    }
+    const liStyles ={
+        padding : "3px"
+    }
+    const navStyles = {
+        backgroundColor : "lightgrey",
+    }
+    return(
+        <header className = "header">
+           
+            <nav className="navbar" style={navStyles}>
+                <ul className="ul" style={ulStyles}>
+                    <li style={liStyles}>Inicio</li>
+                    <li style={liStyles}>Productos</li>
+                    <li style={liStyles}>Carrito</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
