f7a49c7aa7ca58ee37e825e0b74e85712b42ad35
tomylandolfi <tomaslandolfi@gmail.com>
2022-05-10 23:01:30 -0300

    Agregado el contador

--- src/components/ItemListContainer/index.jsx
+++ src/components/ItemListContainer/index.jsx
@@ -1,8 +1,16 @@
+import ItemCount from "../ItemCount/ItemCount";
 import React from "react";
 import Title from "../Title"
 export const ItemListContainer = () => {
+    const onAdd = (quantity) =>{
+      console.log(`Compraste ${quantity} unidades`)
+    }
+  
     return(
-      <Title greeting="Tomas" />  
+      <>
+      <Title greeting="Tomas" />
+      <ItemCount initial={1} stock={5} onAdd={onAdd} />
+      </>  
     )
 }
 
